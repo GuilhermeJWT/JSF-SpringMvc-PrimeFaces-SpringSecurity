a153fd6133c8efb79c91e3c98f837ac56938ca65
Guilherme Santos <guiromanno@gmail.com>
2021-07-08 12:57:23 -0300

    Criando nosso Comparator e Anotação customizada para Pesquisa

---
+++ src/br/com/systemsgs/config/CampoConsultaConfig.java
@@ -0,0 +1,82 @@
+package br.com.systemsgs.config;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public class CampoConsultaConfig implements Serializable, Comparator<CampoConsultaConfig>{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String descricao;
+	private String campoBanco;
+	private Object tipoClass;
+	private Integer principal;
+	
+	@Override
+	public int compare(CampoConsultaConfig o1, CampoConsultaConfig o2) {
+		return ((CampoConsultaConfig)o1).getPrincipal().compareTo(((CampoConsultaConfig)o2).getPrincipal());
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCampoBanco() {
+		return campoBanco;
+	}
+
+	public void setCampoBanco(String campoBanco) {
+		this.campoBanco = campoBanco;
+	}
+
+	public Object getTipoClass() {
+		return tipoClass;
+	}
+
+	public void setTipoClass(Object tipoClass) {
+		this.tipoClass = tipoClass;
+	}
+
+	public Integer getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(Integer principal) {
+		this.principal = principal;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((campoBanco == null) ? 0 : campoBanco.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CampoConsultaConfig other = (CampoConsultaConfig) obj;
+		if (campoBanco == null) {
+			if (other.campoBanco != null)
+				return false;
+		} else if (!campoBanco.equals(other.campoBanco))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return getDescricao();
+	}
+	
+}
